a0f5ea87c63097428ce34ae310b956c573ad017c
Egor Muratov <egormuratov@yahoo.com>
2021-08-24 21:36:16 +0300

    init project

---
+++ src/test/java/org/wikipedia/helpers/Browserstack.java
@@ -0,0 +1,26 @@
+package org.wikipedia.helpers;
+
+
+import org.aeonbits.owner.ConfigFactory;
+import org.wikipedia.config.MobileConfig;
+
+import static io.restassured.RestAssured.given;
+
+public class Browserstack {
+
+    static MobileConfig config = ConfigFactory.create(MobileConfig.class, System.getProperties());
+
+    public static String videoUrl(String sessionId) {
+        return given()
+                .auth().basic(config.getBrowserStackUser(), config.getBrowserStackKey())
+                .when()
+                .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId + ".json")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .extract()
+                .response()
+                .path("automation_session.video_url");
+
+    }
+}
