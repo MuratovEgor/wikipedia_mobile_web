e0e83fb82870ec776af8c2b1666a25bcb0ef9865
Egor Muratov <egormuratov@yahoo.com>
2021-08-29 13:37:13 +0300

    Added simple web test

--- src/test/java/org/wikipedia/tests/web/SearchTests.java
+++ src/test/java/org/wikipedia/tests/web/SearchTests.java
@@ -1,5 +1,6 @@
 package org.wikipedia.tests.web;
 
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,8 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.wikipedia.annotations.Layer;
 import org.wikipedia.tests.TestBase;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -19,8 +22,20 @@ public class SearchTests extends TestBase {
     @Test
     @DisplayName("Successful search in wikipedia android app")
     void searchTest() {
-        step("Open url https://www.wikipedia.org/", () -> {
-            open("https://www.wikipedia.org/");
+        step("Open url https://en.wikipedia.org/wiki/Main_Page", () -> {
+            open("https://en.wikipedia.org/wiki/Main_Page");
         });
+
+        step("Enter Harry Potter into the search bar", () -> {
+            $("#searchInput").val("Harry Potter").pressEnter();
+        });
+
+        step("Check that the title of the article is Harry Potter", ()->{
+            $("#firstHeading").shouldHave(text("Harry Potter"));
+        });
+
+
+
+
     }
 }
