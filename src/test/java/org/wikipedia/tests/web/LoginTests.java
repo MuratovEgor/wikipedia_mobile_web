a9a32a28a3c5e17034c98eca3bb4eb54971dab6e
egormuratov <egormuratov@yahoo.com>
2021-08-30 21:23:33 +0300

    login test

---
+++ src/test/java/org/wikipedia/tests/web/LoginTests.java
@@ -0,0 +1,49 @@
+package org.wikipedia.tests.web;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.wikipedia.annotations.Layer;
+import org.wikipedia.tests.TestBase;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+@Owner("egor.muratov")
+@Layer("web")
+@Feature("Main page")
+@Tag("web")
+public class LoginTests extends TestBase {
+    @Test
+    @DisplayName("Unsuccessful login")
+    void unsuccessfulLogin() {
+        step("Open url Login Page", () ->
+            open("https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page")
+        );
+
+        step("Type test in Login field", () ->
+            $("#wpName1").val("test")
+        );
+
+        step("Type test in Password field", () ->
+            $("#wpPassword1").val("test")
+        );
+
+        step("Click button Log in", () ->
+                $("#wpLoginAttempt").click()
+        );
+
+        step("Check that block Error is visible", () ->
+            $(".errorbox").shouldBe(visible)
+        );
+
+
+    }
+
+}
--- src/test/java/org/wikipedia/tests/web/SearchTests.java
+++ src/test/java/org/wikipedia/tests/web/SearchTests.java
@@ -22,18 +22,18 @@ public class SearchTests extends TestBase {
     @Test
     @DisplayName("Successful search in wikipedia.org")
     void searchTest() {
-        step("Open url https://en.wikipedia.org/wiki/Main_Page", () -> {
-            open("https://en.wikipedia.org/wiki/Main_Page");
-        });
+        step("Open url https://en.wikipedia.org/wiki/Main_Page", () ->
+            open("https://en.wikipedia.org/wiki/Main_Page")
+        );
 
-        step("Enter Harry Potter into the search bar", () -> {
-            $("#searchInput").val("Harry Potter").pressEnter();
-        });
+        step("Enter Harry Potter into the search bar", () ->
+            $("#searchInput").val("Harry Potter").pressEnter()
+        );
 
-        step("Check that the title of the article is Harry Potter", () -> {
-            $("#firstHeading").shouldHave(text("Harry Potter"));
-        });
-
-
+        step("Check that the title of the article is Harry Potter", () ->
+            $("#firstHeading").shouldHave(text("Harry Potter"))
+        );
     }
+
+
 }
